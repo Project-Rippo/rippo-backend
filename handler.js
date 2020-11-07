a5eeee60b1ec9c71f230fcac9a46f809d0855262
Pedro Farias <phjfarias@hotmail.com>
2020-11-07 17:38:10 -0300

    feat: create project template

---
+++ handler.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports.graphqlHandler = async (event, context, callback) => {
+  switch (event.field) {
+    case "hello": {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            message: "Go Serverless v1.0! Your function executed successfully!",
+            input: event,
+          },
+          null,
+          2
+        ),
+      });
+    }
+  }
+};
